f3b5552016f3d64707aa3230c2462793a324714c
bryanidem <bryan.medina.per@gmail.com>
2024-02-13 22:22:39 -0600

    Add changing color schema functionality

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const display = document.querySelector(".display");
+const btnGridResolution = document.querySelector(".btn-grid");
+const modalGridResolution = document.querySelector(".modal-grid-res");
+// const btnModalChangeRes = document.querySelector(".submit-change-res");
+const formChangeRes = document.querySelector(".modal-grid-res > form");
+
+const btnColorMode = document.querySelector(".btn-color");
+const modalColorMode = document.querySelector(".modal-color");
+const formColorMode = document.querySelector(".modal-color > form");
+
+let gridResolution = 32;
+let colorSchema = "gray";
+const colors = {
+  gray: ["#4a5759"],
+  rainbow: ["#9b5de5", "#f15bb5", "#fee440", "#00bbf9", "#00f5d4"],
+  green: ["#354f52", "#354f52", "#52796f", "#84a98c", "#cad2c5"],
+  pop: ["#e4c1f9", "#a9def9", "#d0f4de", "#fcf6bd", "#ff99c8"],
+  vintage: ["#bc4b51", "#5b8e7d", "#f4a259", "#f4e285", "#8cb369"],
+};
+
 let mouseDown = false;
 document.body.onmousedown = () => {
   mouseDown = true;
@@ -7,12 +27,10 @@ document.body.onmouseup = () => {
   mouseDown = false;
 };
 
-const colors = ["#9b5de5", "#f15bb5", "#fee440", "#00bbf9", "#00f5d4"];
-
 const changeColor = (e) => {
   if (mouseDown) {
-    e.target.style.background =
-      colors[Math.floor(Math.random() * colors.length)];
+    const color = colors[colorSchema];
+    e.target.style.background = color[Math.floor(Math.random() * color.length)];
   }
 };
 
@@ -29,25 +47,23 @@ const fillGridWithDivs = (display, gridResolution) => {
   }
 };
 
-const getFormData = (form) => {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const formObject = Object.fromEntries(formData);
-  });
-};
+formChangeRes.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(formChangeRes);
+  const formObject = Object.fromEntries(formData);
+  const newResolution = formObject["resolution"];
+  display.replaceChildren();
+  fillGridWithDivs(display, parseInt(newResolution));
+  modalGridResolution.close();
+});
 
-const changeDisplayRes = (form, display, modal) => {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const formObject = Object.fromEntries(formData);
-    const newResolution = formObject["resolution"];
-    display.replaceChildren();
-    fillGridWithDivs(display, parseInt(newResolution));
-    modal.close();
-  });
-};
+formColorMode.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(formColorMode);
+  const formObject = Object.fromEntries(formData);
+  colorSchema = formObject["colors"];
+  modalColorMode.close();
+});
 
 const openModal = (buttonActivateModal, modal) => {
   buttonActivateModal.addEventListener("click", () => {
@@ -55,21 +71,6 @@ const openModal = (buttonActivateModal, modal) => {
   });
 };
 
-const main = () => {
-  const display = document.querySelector(".display");
-  const btnGridResolution = document.querySelector(".btn-grid");
-  const modalGridResolution = document.querySelector(".modal-grid-res");
-  const btnModalChangeRes = document.querySelector(".submit-change-res");
-  const formChangeRes = document.querySelector(".modal-grid-res > form");
-  let gridResolution = 32;
-
-  fillGridWithDivs(display, gridResolution);
-  openModal(btnGridResolution, modalGridResolution);
-  changeDisplayRes(formChangeRes, display, modalGridResolution);
-
-  // btnGridResolution.addEventListener("click", () => {
-  //   modalGridResolution.showModal();
-  // });
-};
-
-main();
+fillGridWithDivs(display, gridResolution);
+openModal(btnGridResolution, modalGridResolution);
+openModal(btnColorMode, modalColorMode);
